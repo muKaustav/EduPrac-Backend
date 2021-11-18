ca8caa8638c82247bb8c62f13c5bd15601dd4bc3
Kaustav Mukhopadhyay <mu.kaustav@gmail.com>
2021-11-18 17:58:17 +0530

    edit user model

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    userId: { type: String, unique: true, required: true, dropDups: true },
-    username: { type: String, unique: true, required: true, dropDups: true },
-    email: { type: String, unique: true, required: true, dropDups: true },
+    userId: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
     photoURL: String,
-    role: { type: String, unique: true, required: true, dropDups: true },
+    role: { type: String, required: true },
     questions: {
         bookmarkedQuestions: [{ uuid: String }],
         attemptedQuestions: [{
