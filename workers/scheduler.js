2eccf6585a9c9bdb8e9ba0d51719c63cd6bad333
Kaustav Mukhopadhyay <mu.kaustav@gmail.com>
2021-11-22 01:43:55 +0530

    cron edit

--- workers/scheduler.js
+++ workers/scheduler.js
@@ -2,7 +2,7 @@ const cron = require('node-cron')
 const mongoose = require('mongoose')
 const user = require('../models/user')
 
-let resetDailyObjective = (req, res) => {
+let resetDailyObjective = () => {
     const Data = mongoose.model('users', user)
 
     Data.find({}, (err, data) => {
@@ -13,14 +13,11 @@ let resetDailyObjective = (req, res) => {
                 user.dailyObjective = 0
                 user.save()
             })
-            res.send("Daily objectives reset.")
         }
     })
 }
 
-
-
-cron.schedule('48 1 * * *', () => {
+cron.schedule('46 1 * * *', () => {
     // reset daily objective every day at midnight
     resetDailyObjective()
 })
