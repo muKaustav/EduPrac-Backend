d1b87c8f0c24f06767a2fae0c7a5d34de4956e8e
Kaustav Mukhopadhyay <mu.kaustav@gmail.com>
2021-11-23 10:09:45 +0530

    rm breaking commit

--- passport.js
+++ passport.js
@@ -15,7 +15,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.LOCAL_CALLBACK_URL
+    callbackURL: process.env.CALLBACK_URL
 },
     (accessToken, refreshToken, profile, cb) => {
         const Data = mongoose.model('users', user)
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,8 +5,7 @@ const checkAdminLoggedIn = require("../middleware/adminAuth")
 
 router = express.Router()
 
-// router.get("/users", checkAdminLoggedIn, userRoute.getUsers)
-router.get("/users", userRoute.getUsers)
+router.get("/users", checkAdminLoggedIn, userRoute.getUsers)
 router.post("/user", userRoute.postUser)
 router.post("/userQuestion", checkUserLoggedIn, userRoute.postUserQuestion)
 router.get("/getDaily", checkUserLoggedIn, userRoute.getDaily)
